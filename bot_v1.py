88259c4bb4f67e85ccf70a632f2c50f5939ace91
macbook <ikamil522@gmail.com>
2022-10-16 23:08:21 +0300

    Эти ребята любят Полину, больше чем кого либо

---
+++ bot_v1.py
@@ -0,0 +1,29 @@
+#Бот присылает сообщения при нажати на кнопку
+import telebot
+import random
+import time
+
+bot = telebot.TeleBot('BOT_TOKEN')
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Приветики', reply_markup=keyboard1)
+
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    if message.text.lower() == 'комплимент':
+        bot.send_message(message.chat.id, random.choice(compliments))
+    else:
+        bot.send_message(message.chat.id, 'Я тебя не понимаю. Напиши /start.')
+
+keyboard1 = telebot.types.ReplyKeyboardMarkup(True)
+keyboard1.row('Комплимент')
+
+
+compliments = ['это массив комплиментов']
+
+while True:
+    try:
+        bot.polling(none_stop=True)
+    except Exception as e:
+        time.sleep(15)
---
+++ bot_v2.py
@@ -0,0 +1,25 @@
+#Бот присылает сообщения в с рандомным промежутком
+import telebot
+import random
+import time
+from datetime import datetime
+
+bot = telebot.TeleBot('BOT_TOKEN')
+member_id = 'CHAT_ID'
+
+compliments = ['это массив комплиментов']
+timer = [3600, 7200, 5400, 14400]
+
+bot.send_message(member_id, "Текст который отправиться при запуске бота")
+now = datetime.now()
+current_time = now.strftime("%H:%M:%S")
+print('мы запустились в ' + current_time)
+
+while True:
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    bot.send_message(member_id, random.choice(compliments))
+    print('комплимент отправлен в ' + current_time)
+    time.sleep(random.choice(timer))
+
+bot.infinity_polling()
